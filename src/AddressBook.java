d9f4ad37f24e5fa53b57f04795327a1f88bb6378
Bhupendra124 <wadekar.bhupendra6@gmail.com>
2021-08-05 16:02:30 +0530

    ADD: Detail of person

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,5 +1,107 @@
 public class AddressBook {
+
+
+        String firstName;
+        String lastName;
+        String address;
+        String city;
+        String State;
+        String Zip;
+        String phoneNumber;
+        String email;
+
+    public AddressBook(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        State = state;
+        Zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String state) {
+        State = state;
+    }
+
+    public String getZip() {
+        return Zip;
+    }
+
+    public void setZip(String zip) {
+        Zip = zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "AddressBook{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", State='" + State + '\'' +
+                ", Zip='" + Zip + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
     public static void main(String[] args) {
         System.out.println("WELCOME TO ADDRESS BOOK PROGRAM");
+        AddressBook book=new AddressBook("Bhupendra","wadekar","Amla ","Amla","M.P","460551","9893902047","wadekar.bhupendra6@gmail.com");
+        System.out.println("Detail of Person  :"+book);
     }
 }
